8a333b20ecab9e9724b663859ec5993b14539a4a
Geovana <geomscardoso183@gmail.com>
2025-04-01 09:50:02 -0300

    adicionando operadores aritmeticos e logicos

--- src/js/script.js
+++ src/js/script.js
@@ -51,6 +51,50 @@ console.log(numFloat1.toString());
 let numInt=100;
 console.log(numInt.toString());
 
+//Metodos - parte 1
+
+//Length - verifica o tamanho da string
+
+let frase = "O mundo da tecnologia";
+console.log(frase.length)
+
+//indexOf  - retorna um trecho de um texto
+
+let texto = "Programação sustentável";
+console.log(texto.indexOf("ão")) //primeiro
+
+//slice - retorna parte de um texto apontando o inicio e o final
+
+let info = "Processamento de ponta";
+console.log(info.slice(0,18))
+
+//Operadores aritméticos
+
+const num1=10;
+const num2=20;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+//Operadores lógicos
+const num3 = 20;
+const num4 = 30;
+
+console.log(num3 < num4);
+console.log(num3 > num4 && num4 < 50);
+console.log(num3 > num4 || num4 > num3);
+console.log(num4 == num3 || num4 > num3);
+
+
+
+
+
+
+
+
+
 
 
 
